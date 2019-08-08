0f0b55dd1e2fc88d838d58f7a3cfb2ea3769af7e
mapktah <mapktah@yandex.ru>
2019-08-08 12:58:51 +0800

    updates

---
+++ build/lib/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mozg.utils',
+    version='1.0.0',
+    packages=[''],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='mark.tan',
+    author_email='mapktah@yandex.ru',
+    description='Basic Utilities'
+)
