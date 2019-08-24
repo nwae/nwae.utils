6fbfd137ca4b7da4abf548699ce6f2128622c2d3
mapktah <mapktah@yandex.ru>
2019-08-24 09:48:47 +0800

    add new thread group class

---
+++ src/mozg/utils/ThreadGroup.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+
+import threading
+import queue as q
+import time
+import mozg.utils.Log as lg
+from inspect import getframeinfo, currentframe
+import mozg.utils.StringUtils as su
+
+
+#
+# Puts threads to a single group, so we can start/kill everything together
+#
+class ThreadGroup(threading.Thread):
+
+    def __init__(
+            self,
+            thread_group_name
+    ):
+        super(ThreadGroup, self).__init__()
+        self.thread_group_name = thread_group_name
+        self.thread_collection = {}
+        return
+
+    #
+    # Returns True of False, if successful or not
+    #
+    def add_thread(
+            self,
+            new_thread_name,
+            new_thread
+    ):
+        # if type(new_thread) is not threading.Thread:
+        #     errmsg = str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)\
+        #              + ': [' + str(self.thread_group_name) + '] New thread "' + str(new_thread_name)\
+        #              + '" not correct type, instead of type "' + str(type(new_thread)) + '".'
+        #     lg.Log.error(errmsg)
+        #     raise Exception(errmsg)
+
+        key = su.StringUtils.trim(str(new_thread_name))
+        # If existing thread of the same name exists
+        if key == '':
+            errmsg = str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)\
+                     + ': [' + str(self.thread_group_name) + '] Thread name must not be empty!'
+            lg.Log.warning(errmsg)
+            return False
+
+        if key in self.thread_collection.keys():
+            errmsg = str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)\
+                     + ': [' + str(self.thread_group_name) + '] Thread "' + str(new_thread_name)\
+                     + '" already exists in collection!'
+            lg.Log.warning(errmsg)
+            return False
+
+        self.thread_collection[key] = new_thread
+        return True
+
+    def start_thread_group(
+            self
+    ):
+        for th_name in self.thread_collection.keys():
+            th = self.thread_collection[th_name]
+            lg.Log.important(
+                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno) \
+                + ': [' + str(self.thread_group_name) + '] Starting thread "' + str(th_name) + '"..'
+            )
+            th.start()
+
+    def run(self):
+        lg.Log.important(
+            str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno) \
+            + ': [' + str(self.thread_group_name) + '] Starting thread group..'
+        )
+        is_any_thread_dead = False
+        while True:
+            if is_any_thread_dead:
+                # Kill all threads
+                for th_name in self.thread_collection.keys():
+                    th = self.thread_collection[th_name]
+                    lg.Log.info(
+                        str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno) \
+                        + ': [' + str(self.thread_group_name) + '] Joining Thread "' + str(th_name) + '"..'
+                    )
+                    th.join()
+                lg.Log.info(
+                    str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno) \
+                    + ': All threads of thread group "' + str(self.thread_group_name)  + '" joined..'
+                )
+                break
+
+            for th_name in self.thread_collection.keys():
+                th = self.thread_collection[th_name]
+                if not th.isAlive():
+                    lg.Log.info(
+                        str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno) \
+                        + ': [' + str(self.thread_group_name) + '] Thread "' + str(th_name)
+                        + '" no longer alive. Killing everyone else.'
+                    )
+                    is_any_thread_dead = True
+                    break
+
+                if not is_any_thread_dead:
+                    lg.Log.info(
+                        str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno) \
+                        + ': [' + str(self.thread_group_name) + '] All threads OK.'
+                    )
+            time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    class TaskThread(threading.Thread):
+        def __init__(self, thread_name, sleep_time):
+            super(TaskThread, self).__init__()
+            self.thread_name = thread_name
+            self.sleep_time = sleep_time
+
+        def run(self):
+            while True:
+                # Task is to sleep
+                time.sleep(self.sleep_time)
+                print('Sleep done for thread "' + str(self.thread_name) + '"..')
+                break
+    lg.Log.LOGLEVEL = lg.Log.LOG_LEVEL_INFO
+
+    t1 = TaskThread(thread_name='t1', sleep_time=5)
+    t2 = TaskThread(thread_name='t2', sleep_time=500)
+    t3 = TaskThread(thread_name='t3', sleep_time=800)
+
+    tg = ThreadGroup(thread_group_name='tg')
+    tg.add_thread(new_thread_name=t1.thread_name, new_thread=t1)
+    tg.add_thread(new_thread_name=t2.thread_name, new_thread=t2)
+    tg.add_thread(new_thread_name=t3.thread_name, new_thread=t3)
+
+    tg.start_thread_group()
+    tg.start()
+
+    exit(0)
