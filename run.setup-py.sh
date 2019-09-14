a48da156bb165d61096c1e70d9b8d7ebe03ed574
mapktah <mapktah@yandex.ru>
2019-09-14 21:12:18 +0800

    updates

--- run.setup-py.sh
+++ run.setup-py.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+# Script name is the first parameter on command line
+SCRIPT_NAME="$0"
+
+NWAE_UTILS_VERSION=
+
+for keyvalue in "$@"; do
+    echo "[$SCRIPT_NAME] Key value pair [$keyvalue]"
+    IFS='=' # space is set as delimiter
+    read -ra KV <<< "$keyvalue" # str is read into an array as tokens separated by IFS
+
+    if [ "$KV" == "version" ] ; then
+        NWAE_UTILS_VERSION=${KV[1]}
+        echo "[$SCRIPT_NAME] Set version to $NWAE_UTILS_VERSION."
+    fi
+done
 PYTHON=/usr/local/bin/python3.6
 PIP=/usr/local/bin/pip3.6
+
+if [ "$NWAE_UTILS_VERSION" = "" ]; then
+  echo "[$SCRIPT_NAME] Must specify version!"
+  exit 1
+fi
 
 # Clear build folder
 rm -rf ./build/*
@@ -9,8 +29,11 @@ rm -rf ./build/*
 # Create wheel
 $PYTHON src/setup.py bdist_wheel
 
-# Uninstall old mozg
-$PIP uninstall nwae.utils
+# Upload to pypi
+$PYTHON -m twine upload "dist/nwae.utils-$NWAE_UTILS_VERSION-py3-none-any.whl"
 
-# Install back
-$PIP install dist/*.whl
+## Uninstall old mozg
+#$PIP uninstall nwae.utils
+#
+## Install back
+#$PIP install dist/*.whl
--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nwae.utils',
-    version='1.0.0',
+    version='1.1.0',
     packages=[
         'nwae.utils'
     ],
