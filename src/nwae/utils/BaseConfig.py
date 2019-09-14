a3f181cd27f81d5b7ba501078a929ac77c0c0cd9
mapktah <mapktah@yandex.ru>
2019-09-15 07:54:17 +0800

    update

--- src/nwae/utils/BaseConfig.py
+++ src/nwae/utils/BaseConfig.py
@@ -16,8 +16,6 @@ class BaseConfig:
 
     PARAM_CONFIGFILE = 'configfile'
 
-    DEFAULT_RELOAD_EVERY_X_SECS = 300
-
     SINGLETON = {}
 
     #
@@ -25,8 +23,7 @@ class BaseConfig:
     #
     @staticmethod
     def get_cmdline_params_and_init_config_singleton(
-            Derived_Class,
-            reload_every_x_secs = DEFAULT_RELOAD_EVERY_X_SECS
+            Derived_Class
     ):
         # Default values
         pv = {
@@ -58,12 +55,13 @@ class BaseConfig:
         # Instantiate the Derived Class, not this base config
         #
         BaseConfig.SINGLETON[configfile] = Derived_Class(
-            config_file         = configfile,
-            reload_every_x_secs = reload_every_x_secs
+            config_file         = configfile
         )
         return BaseConfig.SINGLETON[configfile]
 
     def get_config(self, param):
+        if self.__is_file_last_updated_time_is_newer():
+            self.reload_config()
         if param in self.param_value.keys():
             return self.param_value[param]
         else:
@@ -75,17 +73,64 @@ class BaseConfig:
 
     def __init__(
             self,
-            config_file,
-            reload_every_x_secs = DEFAULT_RELOAD_EVERY_X_SECS
+            config_file
     ):
         self.config_file = config_file
-        self.last_updated_time = None
-        self.reload_every_x_secs = reload_every_x_secs
+
+        self.__check_file_existence()
+        self.file_updated_time = os.path.getmtime(self.config_file)
+        lg.Log.important(
+            str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+            + ': Config file path "' + str(self.config_file) + '" ok, updated time "'
+            + str(dt.datetime.fromtimestamp(self.file_updated_time).strftime('%Y%m%d %H:%M:%S'))
+            + '".'
+        )
 
         self.param_value = {}
 
         self.__mutex_reload_config = threading.Lock()
         self.reload_config()
+        return
+
+    def __check_file_existence(self):
+        if not os.path.isfile(self.config_file):
+            raise Exception(
+                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + ': Config file path "' + str(self.config_file)
+                + '" is not a valid file path!'
+            )
+        else:
+            lg.Log.info(
+                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + ': Config file path "' + str(self.config_file) + '" OK.'
+            )
+
+    def __is_file_last_updated_time_is_newer(self):
+        try:
+            # Check if file time is newer
+            ftime = os.path.getmtime(self.config_file)
+            is_file_newer = ftime > self.file_updated_time
+
+            if is_file_newer:
+                lg.Log.important(
+                    str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                    + ': Config file "' + str(self.config_file) + '" updated time "'
+                    + str(dt.datetime.fromtimestamp(ftime).strftime('%Y%m%d %H:%M:%S'))
+                    + '", is newer than "'
+                    + str(dt.datetime.fromtimestamp(self.file_updated_time).strftime('%Y%m%d %H:%M:%S'))
+                    + '".'
+                )
+                # Update with new time
+                self.file_updated_time = ftime
+
+            return is_file_newer
+        except Exception as ex:
+            errmsg = \
+                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)\
+                + ': Exception checking file updated time for config "' + str(self.config_file)\
+                + '", exception message: ' + str(ex) + '.'
+            lg.Log.error(errmsg)
+            return False
 
     def set_default_value_if_not_exist(
             self,
@@ -102,34 +147,14 @@ class BaseConfig:
     def reload_config(
             self
     ):
-        tnow = dt.datetime.now()
-
-        if self.last_updated_time is not None:
-            tdif = tnow - self.last_updated_time
-            #
-            # Not yet expired, no need to reload
-            #
-            if tdif.total_seconds() <= self.reload_every_x_secs:
-                lg.Log.info(
-                    str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
-                    + ': Not yet expired ' + str(tdif.total_seconds()) + ' <= ' + str(self.reload_every_x_secs) + '.'
-                )
-                return
-
-        if not os.path.isfile(self.config_file):
-            raise Exception(
-                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
-                + ': Config file path "' + str(self.config_file)
-                + '" is not a valid file path!'
-            )
-        else:
-            lg.Log.info(
-                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
-                + ': Config file path "' + str(self.config_file) + '" OK.'
-            )
-
         try:
+            lg.Log.important(
+                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + ': Config file "' + str(self.config_file) + '" reloading...'
+            )
+
             self.__mutex_reload_config.acquire()
+
             # Param-Values
             tmp_param_value = {}
 
@@ -183,24 +208,23 @@ if __name__ == '__main__':
     # config_file = '/usr/local/git/nwae/nwae/app.data/config/nwae.cf.local'
     import time
     bconfig = BaseConfig.get_cmdline_params_and_init_config_singleton(
-        reload_every_x_secs = 2
+        Derived_Class = BaseConfig
     )
-    bconfig2 = BaseConfig.get_cmdline_params_and_init_config_singleton()
+    bconfig2 = BaseConfig.get_cmdline_params_and_init_config_singleton(
+        Derived_Class = BaseConfig
+    )
     time.sleep(1)
-    bconfig.reload_config()
-    time.sleep(1.1)
     bconfig.reload_config()
 
     bconfig = BaseConfig(
-        config_file = '/usr/local/git/nwae/nwae/app.data/config/nwae.cf.local',
-        reload_every_x_secs = 5
+        config_file = '/usr/local/git/nwae/nwae/app.data/config/nwae.cf.local'
     )
 
-    time.sleep(2)
     bconfig.reload_config()
 
-    time.sleep(4)
-    bconfig.reload_config()
+    while True:
+        time.sleep(1)
+        bconfig.reload_config()
 
-    print(bconfig.get_config(param='topdir'))
-    print(bconfig.get_config(param='abc'))
+        print(bconfig.get_config(param='topdir'))
+        print(bconfig.get_config(param='abc'))
