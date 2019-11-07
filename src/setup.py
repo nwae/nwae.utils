d91403f42b72d39da7f1b54dd4269a3d907b30da
mapktah <mapktah@yandex.ru>
2019-11-07 10:49:40 +0800

    version 1.3.7

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nwae.utils',
-    version='1.3.6',
+    version='1.3.7',
     packages=[
         'nwae.utils'
     ],
@@ -10,6 +10,6 @@ setup(
     url='',
     license='',
     author='nwae',
-    author_email='705270564@qq.com',
+    author_email='m5251@naver.com',
     description='Basic & Useful Utilities'
 )
