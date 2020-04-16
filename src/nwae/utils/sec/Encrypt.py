67bc6518855b212df47e2cf9f773f0f0754a0c25
Mark <mapktah@ya.ru>
2020-04-16 16:32:57 +0800

    return base64 for encrypt/decrypt

--- src/nwae/utils/sec/Encrypt.py
+++ src/nwae/utils/sec/Encrypt.py
@@ -2,9 +2,12 @@
 
 import os
 import random
-# pycryptodome
+# pip install pycryptodome
 from Crypto.Cipher import AES
 from nwae.utils.Log import Log
+from inspect import getframeinfo, currentframe
+from base64 import b64encode, b64decode
+import nwae.utils.UnitTest as ut
 
 
 STR_ENCODING = 'utf-8'
@@ -35,16 +38,20 @@ class AES_Encrypt:
             # 16 or 32 byte key
             key,
             nonce = None,
-            mode = AES.MODE_EAX
+            mode = AES.MODE_EAX,
+            encoding = 'utf-8'
     ):
         self.key = key
         Log.debug('Using key ' + str(str(self.key)) + '. Size = ' + str(len(self.key)) + '.')
         self.cipher_mode = mode
         if nonce is None:
-            nonce = AES_Encrypt.generate_random_bytes(size=AES_Encrypt.SIZE_NONCE, printable=True)
+            nonce = key[0:16]
+            #nonce = AES_Encrypt.generate_random_bytes(size=AES_Encrypt.SIZE_NONCE, printable=True)
 
         self.nonce = nonce
         Log.debug('Using nonce "' + str(self.nonce) + '". Size = ' + str(len(self.nonce)))
+
+        self.encoding = encoding
         return
 
     def encode(
@@ -56,21 +63,28 @@ class AES_Encrypt:
             if self.cipher_mode == AES.MODE_EAX:
                 cipher = AES.new(key=self.key, mode=self.cipher_mode, nonce=self.nonce)
                 ciphertext, tag = cipher.encrypt_and_digest(data)
+                return b64encode(ciphertext).decode(self.encoding)
             elif self.cipher_mode == AES.MODE_CBC:
                 # 1-16, make sure not 0, other wise last byte will not be block length
                 length = AES_Encrypt.DEFAULT_BLOCK_SIZE_AES_CBC - (len(data) % AES_Encrypt.DEFAULT_BLOCK_SIZE_AES_CBC)
                 # Pad data with the original length, so when we decrypt we can just take data[-1]
                 # as length of data block
                 data += bytes(chr(length), encoding=STR_ENCODING) * length
-                Log.debug('Padded length = ' + str(length))
+                Log.debugdebug(
+                    str(__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                    + ': Padded length = ' + str(length)
+                )
                 cipher = AES.new(key=self.key, mode=self.cipher_mode, iv=self.nonce)
                 ciphertext = cipher.encrypt(data)
+                return b64encode(ciphertext).decode(self.encoding)
             else:
-                raise Exception('Unsupported mode "' + str(self.cipher_mode) + '".')
-
-            return ciphertext
+                raise Exception(
+                    str(__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                    + ': Unsupported mode "' + str(self.cipher_mode) + '".'
+                )
         except Exception as ex:
-            errmsg = 'Error encoding data "' + str(data) + '" using AES ". Exception: ' + str(ex)
+            errmsg = str(__name__) + ' ' + str(getframeinfo(currentframe()).lineno) \
+                    + ': Error encoding data "' + str(data) + '" using AES ". Exception: ' + str(ex)
             Log.error(errmsg)
             return None
 
@@ -81,57 +95,95 @@ class AES_Encrypt:
         try:
             if self.cipher_mode == AES.MODE_EAX:
                 cipher = AES.new(key=self.key, mode=self.cipher_mode, nonce=self.nonce)
-                data = cipher.decrypt(ciphertext)
+                cipherbytes = b64decode(ciphertext.encode(self.encoding))
+                data = cipher.decrypt(cipherbytes)
             elif self.cipher_mode == AES.MODE_CBC:
                 cipher = AES.new(key=self.key, mode=self.cipher_mode, iv=self.nonce)
-                data = cipher.decrypt(ciphertext)
-                Log.debug('Decrypted data length = ' + str(len(data)) + ', modulo 16 = ' + str(len(data) % 128/8))
+                cipherbytes = b64decode(ciphertext.encode(self.encoding))
+                data = cipher.decrypt(cipherbytes)
+                Log.debugdebug(
+                    str(__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                    + 'Decrypted data length = ' + str(len(data)) + ', modulo 16 = ' + str(len(data) % 128/8)
+                )
                 # Remove last x bytes encoded in the padded bytes
                 data = data[:-data[-1]]
             else:
-                raise Exception('Unsupported mode "' + str(self.cipher_mode) + '".')
+                raise Exception(
+                    str(__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                    + ': Unsupported mode "' + str(self.cipher_mode) + '".'
+                )
 
             return str(data, encoding=STR_ENCODING)
         except Exception as ex:
-            errmsg = 'Error decoding data "' + str(ciphertext) + '" using AES ". Exception: ' + str(ex)
+            errmsg = str(__name__) + ' ' + str(getframeinfo(currentframe()).lineno) \
+                    + ': Error decoding data "' + str(ciphertext) + '" using AES ". Exception: ' + str(ex)
             Log.error(errmsg)
             return None
 
 
-if __name__ == '__main__':
-    Log.LOGLEVEL = Log.LOG_LEVEL_DEBUG_1
-    long_str = ''
-    for i in range(10000):
-        long_str += random.choice(AES_Encrypt.CHARS_STR)
-    sentences = [
-        '니는 먹고 싶어',
-        'Дворянское ГНЕЗДО',
-        '没问题 大陆 经济',
-        '存款方式***2019-12-11 11：38：46***',
-        '1234567890123456',
-        long_str
-    ]
+class EncryptUnitTest:
+    def __init__(self, ut_params):
+        return
 
-    key = b'Sixteen byte key'
-    nonce = b'0123456789xxyyzz'
-    # aes_obj = AES_Encrypt(key=AES_Encrypt.generate_random_bytes(size=32, printable=True))
-    aes_obj = AES_Encrypt(
-        key   = key+key,
-        mode  = AES.MODE_CBC,
-        nonce = nonce
-    )
-    for s in sentences:
-        print('Encrypting "' + str(s) + '"')
-        data_bytes = bytes(s.encode(encoding=STR_ENCODING))
-        print('Data length in bytes = ' + str(len(data_bytes)))
-        ciphertext = aes_obj.encode(
-            data = data_bytes
+    def run_unit_test(self):
+        res_final = ut.ResultObj(count_ok=0, count_fail=0)
+
+        long_str = ''
+        for i in range(10000):
+            long_str += random.choice(AES_Encrypt.CHARS_STR)
+        sentences = [
+            '니는 먹고 싶어',
+            'Дворянское ГНЕЗДО',
+            '没问题 大陆 经济',
+            '存款方式***2019-12-11 11：38：46***',
+            '1234567890123456',
+            long_str
+        ]
+
+        key = b'Sixteen byte key'
+        nonce = b'0123456789xxyyzz'
+        # aes_obj = AES_Encrypt(key=AES_Encrypt.generate_random_bytes(size=32, printable=True))
+        aes_obj = AES_Encrypt(
+            key=key + key,
+            mode=AES.MODE_CBC,
+            nonce=nonce
         )
-        print('Encrypted as "' + str(ciphertext) + '"')
-        plaintext = aes_obj.decode(ciphertext = ciphertext)
-        print('Decrypted as "' + plaintext + '"')
+        for s in sentences:
+            Log.debug('Encrypting "' + str(s) + '"')
+            data_bytes = bytes(s.encode(encoding=STR_ENCODING))
+            Log.debug('Data length in bytes = ' + str(len(data_bytes)))
+            ciphertext = aes_obj.encode(
+                data=data_bytes
+            )
+            Log.debug('Encrypted as "' + str(ciphertext) + '"')
 
-        if s == plaintext:
-            print('PASS')
-        else:
-            raise Exception('FAIL')
+            plaintext = aes_obj.decode(ciphertext=ciphertext)
+            Log.debug('Decrypted as "' + plaintext + '"')
+
+            res_final.update_bool(res_bool=ut.UnitTest.assert_true(
+                observed = plaintext,
+                expected = s,
+                test_comment = '"' + str(s) + '" encrypted to "' + str(ciphertext)
+                               + '", decrypted back to "' + str(plaintext)
+            ))
+
+        return res_final
+
+
+if __name__ == '__main__':
+    res = EncryptUnitTest(ut_params=None).run_unit_test()
+
+    Log.LOGLEVEL = Log.LOG_LEVEL_DEBUG_2
+
+    key_str = 'Sixteen byte key'
+    aes_test = AES_Encrypt(
+        key   = key_str.encode('utf-8'),
+        mode  = AES.MODE_CBC
+    )
+
+    s = '0077788'
+    ciphertext = 'mRRYsZfIas4UwjlIy5wB7w=='
+    plaintext = aes_test.decode(
+        ciphertext = ciphertext
+    )
+    print(plaintext)
