c0f9a58914ee20ff250506bbf5e4a39b4ff2a3ee
Mark <mapktah@ya.ru>
2020-06-20 13:36:13 +0800

    log level

--- src/nwae/utils/Cleanup.py
+++ src/nwae/utils/Cleanup.py
@@ -32,14 +32,14 @@ class Cleanup:
     ):
         tnow = time.time()
         tnow_str = format(dt.datetime.fromtimestamp(tnow))
-        lg.Log.debug(
+        lg.Log.important(
             str(self.__class__) + str(getframeinfo(currentframe()).lineno)
             + ': Removing old files from folder "' + self.folder + '" older than '
             + str(self.max_age_secs) + ' secs...'
         )
         try:
             farr = os.listdir(self.folder)
-            lg.Log.debug('Files in folder: ' + str(farr))
+            lg.Log.important('Files in folder: ' + str(farr))
             for file in farr:
                 if not re.search(pattern=self.regex, string=file):
                     # lg.Log.info('Ignoring file "' + str(file) + '", not matching regex "' + str(self.regex) + '".')
