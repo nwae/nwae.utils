ff286be2b37df8824f9e84c031d4bb08b740d413
Mark <mapktah@ya.ru>
2020-06-27 10:25:28 +0800

    update

--- src/nwae/utils/audio/AudioPlay.py
+++ src/nwae/utils/audio/AudioPlay.py
@@ -5,6 +5,8 @@
 #  > pip install PyAudio
 import pyaudio
 import wave
+from nwae.utils.Log import Log
+from inspect import getframeinfo, currentframe
 
 
 class AudioPlay:
@@ -16,10 +18,12 @@ class AudioPlay:
         self.audio_filepath = audio_filepath
         return
 
-    def play(self):
-        # define stream chunk
-        chunk = 1024
-
+    def play(
+            self,
+            # Stream chunk
+            chunk = 1024,
+            n_chunks = None
+    ):
         # open a wav format music
         f = wave.open(self.audio_filepath, "rb")
         # instantiate PyAudio
@@ -33,9 +37,18 @@ class AudioPlay:
         data = f.readframes(chunk)
 
         # play stream
+        i = 0
         while data:
             stream.write(data)
             data = f.readframes(chunk)
+            i += 1
+            Log.debug('Chunk #' + str(i))
+            if n_chunks and i >= n_chunks:
+                Log.info(
+                    str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                    + ': Done playing ' + str(n_chunks) + ' chunks.'
+                )
+                break
 
             # stop stream
         stream.stop_stream()
@@ -46,9 +59,12 @@ class AudioPlay:
 
 
 if __name__ == '__main__':
-    audio_filepath = '/usr/local/git/nwae/nwae.lang/app.data/voice-recordings/hi.wav'
+    Log.LOGLEVEL = Log.LOG_LEVEL_DEBUG_1
+    audio_filepath = '/usr/local/git/nwae/nwae.lang/app.data/voice-recordings/OSR_cn_000_0072_8k.wav'
 
     AudioPlay(
         audio_filepath = audio_filepath
-    ).play()
+    ).play(
+        n_chunks = 50
+    )
     exit(0)
