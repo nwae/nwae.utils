1ce4daeb4a8a0adb5915e214fa4ee04a2b27d50f
Mark <mapktah@ya.ru>
2020-06-27 17:01:48 +0800

    update

---
+++ src/nwae/utils/audio/AudioUtils.py
@@ -0,0 +1,152 @@
+# Need ffmpeg
+# > brew install ffmpeg
+from pydub import AudioSegment
+import re
+from nwae.utils.Log import Log
+from inspect import getframeinfo, currentframe
+import wave
+# To get audio from microphone
+#  > brew install portaudio
+#  > pip install PyAudio
+import pyaudio
+
+
+class AudioUtils:
+
+    def __init__(
+            self
+    ):
+        return
+
+    def get_audio_filepath_extension(
+            self,
+            filepath
+    ):
+        return re.sub(pattern='(.*[.])([a-zA-Z0-9]+$)', repl='\\2', string=filepath)
+
+    def get_audio_file_properties(
+            self,
+            wav_filepath
+    ):
+        assert self.get_audio_filepath_extension(filepath=wav_filepath) == 'wav',\
+            'Audio file "' + str(wav_filepath) + '" must be wav file'
+
+        try:
+            p = pyaudio.PyAudio()
+
+            with wave.open(wav_filepath, "rb") as wave_file:
+                format = p.get_format_from_width(wave_file.getsampwidth())
+                nchannels = wave_file.getnchannels()
+                frame_rate = wave_file.getframerate()
+
+                return format, nchannels, frame_rate
+        except Exception as ex:
+            raise Exception(
+                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + ': Could not get frame rate from "' + str(wav_filepath) + '". Exception: ' + str(ex)
+            )
+
+    def play_wav(
+            self,
+            wav_filepath,
+            # Stream chunk
+            chunk = 1024,
+            n_chunks = 0
+    ):
+        assert self.get_audio_filepath_extension(filepath=wav_filepath) == 'wav',\
+            'Audio file "' + str(wav_filepath) + '" must be wav file'
+
+        # open a wav format music
+        f = wave.open(wav_filepath, "rb")
+        # instantiate PyAudio
+        p = pyaudio.PyAudio()
+        # open stream
+        stream = p.open(
+            format   = p.get_format_from_width(f.getsampwidth()),
+            channels = f.getnchannels(),
+            rate     = f.getframerate(),
+            output   = True
+        )
+        # read data
+        data = f.readframes(chunk)
+
+        # play stream
+        i = 0
+        while data:
+            stream.write(data)
+            data = f.readframes(chunk)
+            i += 1
+            Log.debug('Chunk #' + str(i))
+            if n_chunks and i >= n_chunks:
+                Log.info(
+                    str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                    + ': Done playing ' + str(n_chunks) + ' chunks.'
+                )
+                break
+
+            # stop stream
+        stream.stop_stream()
+        stream.close()
+
+        # close PyAudio
+        p.terminate()
+        return
+
+    #
+    # Convert between audio formats mp3, m4a, wav, etc.
+    #
+    def convert_format(
+            self,
+            filepath,
+            to_format = 'wav'
+    ):
+        file_extension = self.get_audio_filepath_extension(filepath=filepath)
+        filepath_converted = re.sub(pattern='[.][a-zA-Z0-9]+$', repl='.wav', string=filepath)
+        Log.info(
+            str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+            + ': Convert "' + str(filepath) + '" with extension "' + str(file_extension)
+            + '" New filepath "' + str(filepath_converted) + '"'
+        )
+        try:
+            track = AudioSegment.from_file(
+                file   = filepath,
+                format = file_extension
+            )
+            Log.info(
+                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + ': Converting "' + str(filepath)
+                + '" to "' + str(filepath_converted) + '"..'
+            )
+            file_handle = track.export(filepath_converted, format=to_format)
+            file_handle.close()
+            Log.info(
+                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + ': Successful Conversion from "' + str(filepath)
+                + '" to "' + str(filepath_converted) + '"..'
+            )
+            return filepath_converted
+        except Exception as ex:
+            raise Exception(
+                str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + ': Exception converting "' + str(filepath)
+                + '" to "' + str(filepath_converted)
+                + '": ' + str(ex)
+            )
+
+
+if __name__ == '__main__':
+    audio_file = '/usr/local/git/nwae/nwae.lang/app.data/voice-recordings/Lenin_-_In_Memory_Of_Sverdlov.ogg.mp3'
+
+    obj = AudioUtils()
+
+    audio_file_wav = obj.convert_format(
+        filepath  = audio_file,
+        to_format = 'wav'
+    )
+    # print('Frame Rate = ' + str(obj.get_audio_file_properties(wav_filepath=obj.get_audio_filepath())))
+    print(
+        'Format, Channels, Frame Rate = '
+        + str(obj.get_audio_file_properties(wav_filepath=audio_file_wav))
+    )
+
+    exit(0)
