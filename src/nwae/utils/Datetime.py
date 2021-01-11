322df63170a64527a51889ddd7c6658779a6e815
Mark <mapktah@ya.ru>
2021-01-11 10:52:06 +0800

    update

--- src/nwae/utils/Datetime.py
+++ src/nwae/utils/Datetime.py
@@ -8,6 +8,20 @@ from datetime import datetime, timedelta
 class Datetime:
 
     @staticmethod
+    # Convert time unit to integer
+    def count_months(
+            # In datetime type
+            x,
+            ref_year = 2021,
+            ref_month = 1,
+            ref_day = 1
+    ):
+        refdate = datetime(year=ref_year, month=ref_month, day=ref_day)
+        dif_years = x.year - refdate.year
+        dif_months = x.month - refdate.month
+        return dif_years * 12 + dif_months
+
+    @staticmethod
     def offset_date_by_month(
             d,
             n = 1
@@ -29,8 +43,14 @@ class Datetime:
 if __name__ == '__main__':
     now = datetime.now()
     print(now)
-    n = 13
-    print('+13 months: ' + str(Datetime.offset_date_by_month(d=now, n=n)))
-    print('-13 months: ' + str(Datetime.offset_date_by_month(d=now, n=-n)))
+    n = 5
+    now_plus_offset = Datetime.offset_date_by_month(d=now, n=n)
+    now_minus_offset = Datetime.offset_date_by_month(d=now, n=-n)
+    print('+5 months: ' + str(now_plus_offset))
+    print('-5 months: ' + str(now_minus_offset))
     print(now)
+
+    print('Count months "' + str(now) + '" = ' + str(Datetime.count_months(x=now)))
+    print('Count months +5 "' + str(now_plus_offset) + '" = ' + str(Datetime.count_months(x=now_plus_offset)))
+    print('Count months -5 "' + str(now_minus_offset) + '" = ' + str(Datetime.count_months(x=now_minus_offset)))
     exit(0)
